ea8174357f248f9aaa4154983a42f4df663ac397
Ramidu Shenal <it24103024@my.sliit.lk>
2026-09-17 11:44:02 +0530

    Add files via upload

---
+++ Q2/IT24103024Lab3Q2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class IT24103024Lab3Q2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the monthly salary: ");
+        double salary = input.nextDouble();
+        System.out.print("Enter the number of OT hours: ");
+        double otHours = input.nextDouble();
+        System.out.print("Enter the OT hourly rate: ");
+        double otRate = input.nextDouble();
+
+        double otAmount = otHours * otRate;
+        double totalSalary = salary + otAmount;
+
+        System.out.println();
+        System.out.println("The total salary including OT is: " + totalSalary);
+    }
+}
